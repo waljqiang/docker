912bfe071789bec936e0116d0846068d145f72fc
waljqiang <waljqiang@163.com>
2020-07-08 15:24:44 +0800

    12

--- cloudnetlot/script/cloudnetlotdatadeploy.sh
+++ cloudnetlot/script/cloudnetlotdatadeploy.sh
@@ -17,9 +17,9 @@ if [ -f "${BASE}/cloudnetlotdata/mysql/data/mysqldata.tar.gz" ];then
     tar zxf ${BASE}/cloudnetlotdata/mysql/data/mysqldata.tar.gz -C ${BASE}/cloudnetlotdata/mysql/data/
     rm -rf ${BASE}/cloudnetlotdata/mysql/data/mysqldata.tar.gz
 fi
-if [ -f "${BASE}/cloudnetlotdata/emqttd/emqttdconf.tar.gz" ];then
-    tar zxf ${BASE}/cloudnetlotdata/emqttd/emqttdconf.tar.gz -C ${BASE}/cloudnetlotdata/emqttd/
-    rm -rf ${BASE}/cloudnetlotdata/emqttd/emqttdconf.tar.gz
+if [ -f "${BASE}/cloudnetlotdata/emqx/emqxconf.tar.gz" ];then
+    tar zxf ${BASE}/cloudnetlotdata/emqx/emqxconf.tar.gz -C ${BASE}/cloudnetlotdata/emqx/
+    rm -rf ${BASE}/cloudnetlotdata/emqx/emqxconf.tar.gz
 fi
 echo "------------------------------清理cloudnetlotdata环境缓存--------------------------"
 tmp1=`docker ps | grep cloudnetlotdata`
@@ -52,7 +52,7 @@ fi
 echo "----------------------------------运行cloudnetlotdata容器--------------------------"
 
 if [ "$2" = "true" ];then
-    docker run --name cloudnetlotdata -p 9094:3306 -p 9095:6379 -p 9096:1883 -p 9097:8084 -p 9098:8083 -p 9099:18083 -v ${BASE}/cloudnetlotdata/mysql/conf:/etc/mysql -v ${BASE}/cloudnetlotdata/mysql/data:/var/lib/mysql -v ${BASE}/cloudnetlotdata/redis/:/etc/redis -v ${BASE}/cloudnetlotdata/emqttd:/etc/emqttd -v /etc/localtime:/etc/localtime:ro --restart=no -it -d ${imagename}
+    docker run --name cloudnetlotdata -p 9094:3306 -p 9095:6379 -p 9096:1883 -p 9097:8084 -p 9098:8083 -p 9099:18083 -v ${BASE}/cloudnetlotdata/mysql/conf:/etc/mysql -v ${BASE}/cloudnetlotdata/mysql/data:/var/lib/mysql -v ${BASE}/cloudnetlotdata/redis/:/etc/redis -v ${BASE}/cloudnetlotdata/emqx:/etc/emqx -v /etc/localtime:/etc/localtime:ro --restart=no -it -d ${imagename}
 else
     docker run --name cloudnetlotdata -p 9094:3306 -p 9095:6379 -p 9096:1883 -p 9097:8084 -p 9098:8083 -p 9099:18083 -v ${BASE}/cloudnetlotdata/mysql/data:/var/lib/mysql-v /etc/localtime:/etc/localtime:ro --restart=no -it -d ${imagename}
 fi
