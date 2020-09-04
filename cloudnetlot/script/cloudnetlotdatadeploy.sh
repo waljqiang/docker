f0093454a9a695ca4d1f70b2547a2ab4dc517368
waljqiang <waljqiang@163.com>
2020-09-04 10:22:09 +0800

    修复配置

--- cloudnetlot/script/cloudnetlotdatadeploy.sh
+++ cloudnetlot/script/cloudnetlotdatadeploy.sh
@@ -50,9 +50,9 @@ else
     docker pull ${imagename}
 fi
 echo "----------------------------------运行cloudnetlotdata容器--------------------------"
-
+echo ${imagename}
 if [ "$2" = "true" ];then
-    docker run --name cloudnetlotdata -p 9094:3306 -p 9095:6379 -p 9096:1883 -p 9097:8084 -p 9098:8083 -p 9099:18083 -v ${BASE}/cloudnetlotdata/mysql/conf:/etc/mysql -v ${BASE}/cloudnetlotdata/mysql/data:/var/lib/mysql -v ${BASE}/cloudnetlotdata/redis/:/etc/redis -v ${BASE}/cloudnetlotdata/emqx:/etc/emqx -v /etc/localtime:/etc/localtime:ro --restart=no -it -d ${imagename}
+    docker run --name cloudnetlotdata -p 9094:3306 -p 9095:6379 -p 9096:1883 -p 9097:8084 -p 9098:8083 -p 9099:18083 -v ${BASE}/cloudnetlotdata/mysql/conf:/etc/mysql -v ${BASE}/cloudnetlotdata/mysql/data:/var/lib/mysql -v ${BASE}/cloudnetlotdata/redis/:/etc/redis -v ${BASE}/cloudnetlotdata/emqx:/etc/emqx -v /etc/localtime:/etc/localtime:ro --restart=no -it -d ${imagename} && docker exec -it cloudnetlotdata /bin/bash -c 'sleep 20 && emqx_ctl admins passwd admin 123456 && emqx_ctl plugins reload emqx_auth_mysql'
 else
-    docker run --name cloudnetlotdata -p 9094:3306 -p 9095:6379 -p 9096:1883 -p 9097:8084 -p 9098:8083 -p 9099:18083 -v ${BASE}/cloudnetlotdata/mysql/data:/var/lib/mysql-v /etc/localtime:/etc/localtime:ro --restart=no -it -d ${imagename}
+    docker run --name cloudnetlotdata -p 9094:3306 -p 9095:6379 -p 9096:1883 -p 9097:8084 -p 9098:8083 -p 9099:18083 -v ${BASE}/cloudnetlotdata/mysql/data:/var/lib/mysql -v /etc/localtime:/etc/localtime:ro --restart=no -it -d ${imagename} && docker exec -it cloudnetlotdata /bin/bash -c 'sleep 20 && emqx_ctl admins passwd admin 123456 && emqx_ctl plugins reload emqx_auth_mysql'
 fi
---
+++ cloudnetlot/script/codedeploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+root_path="/usr/local/cloudnetlot/www"
+cloudnetlot=`find $root_path -name 'cloudnetlot-*.tar.gz'`
+cloudnetlotdaemon=`find $root_path -name 'cloudnetlotdaemon-*.tar.gz'`
+
+if [ ! -z "$cloudnetlot" ];then
+    echo "************************部署云平台代码***************************"
+    if [ ! -d $root_path/cloudnetlot ];then
+	mkdir $root_path/cloudnetlot
+    fi
+    tar zxf $cloudnetlot -C $root_path/cloudnetlot
+    chown -R www-data:www-data $root_path/cloudnetlot
+    chmod -R 755 $root_path/cloudnetlot
+    rm -f $cloudnetlot
+fi
+
+if [ ! -z "$cloudnetlotdaemon" ];then
+    echo "************************部署云平台服务层代码**********************"
+    if [ ! -d $root_path/cloudnetlotdaemon ];then
+	mkdir $root_path/cloudnetlotdaemon
+    fi
+    tar zxf $cloudnetlotdaemon -C $root_path/cloudnetlotdaemon
+    chown -R www-data:www-data $root_path/cloudnetlotdaemon
+    chmod -R 755 $root_path/cloudnetlotdaemon
+    rm -f $cloudnetlotdaemon
+fi
+
+echo "SUCCESS"
